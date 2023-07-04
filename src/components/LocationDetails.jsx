6b9e52f71bc4fa144ad60ba3e4f62259f333fb38
Henri Parviainen <henri.parviainen@icloud.com>
2023-07-04 21:47:18 +0300

    Added new locations

--- src/components/LocationDetails.jsx
+++ src/components/LocationDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from '../styles/LocationDetails.module.css';
 import { useEffect, useState } from 'react';
 import { marked } from 'marked';
+import locationMark from '../../public/images/location-marker-icon.png';
 import DOMPurify from 'dompurify';
 
 marked.setOptions({
@@ -28,6 +29,7 @@ export const LocationDetails = ({ location, closeHandler }) => {
 
   useEffect(() => {
     readTextFile(`./${location.markdown}`);
+    markup.replace('coordinates', location.latlong);
     let clean = DOMPurify.sanitize(
       marked.parse(markup.replace(/&#13;/g, '<br>'))
     );
@@ -53,7 +55,25 @@ export const LocationDetails = ({ location, closeHandler }) => {
           <path d='m6 6 12 12' />
         </svg>
       </span>
-
+      <span className={styles.coordinates}>
+        {' '}
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          stroke-width='2'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+          class='lucide lucide-map-pin'
+        >
+          <path d='M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z' />
+          <circle cx='12' cy='10' r='3' />
+        </svg>
+        {location.latlong[0]} , {location.latlong[1]}
+      </span>
       <div class='container'>
         <div id='preview'></div>
       </div>
